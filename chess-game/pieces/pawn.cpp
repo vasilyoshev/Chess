3e31820f2d86a7e1538f585edba2ecaaa526e947
draawn <vasil.yoshev@gmail.com>
2017-01-10 12:12:34 +0200

    Pawn first move added

--- chess-game/pieces/pawn.cpp
+++ chess-game/pieces/pawn.cpp
@@ -2,15 +2,28 @@
 
 Pawn::Pawn(Color color)
     :Piece(color,ptPawn) {
+    has_moved = false;
 }
 
 std::vector<Coordinate> Pawn::getPossibleMoves(Coordinate currentPosition) {
     std::vector<Coordinate> result;
 
     if(getColor() == cBlack) {
-        result.push_back(Coordinate(currentPosition.getRow() + 1, currentPosition.getColumn()));
+        if (has_moved) {
+            result.push_back(Coordinate(currentPosition.getRow() + 1, currentPosition.getColumn()));
+        } else {
+            result.push_back(Coordinate(currentPosition.getRow() + 2, currentPosition.getColumn()));
+            result.push_back(Coordinate(currentPosition.getRow() + 1, currentPosition.getColumn()));
+            has_moved = true;
+        }
     } else {
-        result.push_back(Coordinate(currentPosition.getRow() - 1, currentPosition.getColumn()));
+        if (has_moved) {
+            result.push_back(Coordinate(currentPosition.getRow() - 1, currentPosition.getColumn()));
+        } else {
+            result.push_back(Coordinate(currentPosition.getRow() - 2, currentPosition.getColumn()));
+            result.push_back(Coordinate(currentPosition.getRow() - 1, currentPosition.getColumn()));
+            has_moved = true;
+        }
     }
 
     return result;
--- chess-game/pieces/pawn.h
+++ chess-game/pieces/pawn.h
@@ -4,6 +4,9 @@
 #include "piece.h"
 
 class Pawn : public Piece {
+private:
+    bool has_moved;
+
 public:
     Pawn(Color color);
     std::vector<Coordinate> getPossibleMoves(Coordinate currentPosition);
