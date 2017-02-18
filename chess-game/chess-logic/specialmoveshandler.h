ae9fa8db2ad222a26ab30db26a9af9e5d404a0a2
draawn <vasil.yoshev@gmail.com>
2017-02-18 17:48:42 +0200

    Small correction

--- chess-game/chess-logic/specialmoveshandler.h
+++ chess-game/chess-logic/specialmoveshandler.h
@@ -10,10 +10,6 @@ private:
     static std::vector<Coordinate> filterInvalidMoves(const State& state, std::vector< std::vector<Coordinate> >& abstractMoves, const Color& attackingColor);
 
     static void getSpecialMoves(King *king, const State &state, std::vector<Coordinate> &abstractMoves, Coordinate kingCoordinate);
-    static void getSpecialMoves(Queen *queen, const State &state, std::vector<Coordinate> &abstractMoves);
-    static void getSpecialMoves(Rook *rook, const State &state, std::vector<Coordinate> &abstractMoves);
-    static void getSpecialMoves(Bishop *bishop, const State &state, std::vector<Coordinate> &abstractMoves);
-    static void getSpecialMoves(Knight *knight, const State &state, std::vector<Coordinate> &abstractMoves);
     static void getSpecialMoves(Pawn *pawn, const State &state, std::vector<Coordinate> &abstractMoves, Coordinate pawnCoordinate);
 
 public:
