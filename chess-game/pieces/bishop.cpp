6d6472b04f8563855ea26e7ac752ae686bd5e240
draawn <vasil.yoshev@gmail.com>
2017-02-18 18:19:55 +0200

    Removed more redundant code.

--- chess-game/pieces/bishop.cpp
+++ chess-game/pieces/bishop.cpp
@@ -1,6 +1,5 @@
 #include "bishop.h"
 
-#define min(a,b) (a<b ? a : b)
 /**
  * @brief Bishop::Bishop
  */
@@ -18,44 +17,44 @@ Bishop::Bishop(Color color)
  * @return vector of vectors, where every vector holds all moves in a given direction.
  */
 std::vector< std::vector<Coordinate> > Bishop::getPossibleMoves(Coordinate currentPosition) {
-    std::vector<Coordinate> upLeft;
-    std::vector<Coordinate> upRight;
-    std::vector<Coordinate> downLeft;
-    std::vector<Coordinate> downRight;
+    std::vector<Coordinate> upperLeft;
+    std::vector<Coordinate> upperRight;
+    std::vector<Coordinate> lowerLeft;
+    std::vector<Coordinate> lowerRight;
 
     int row = currentPosition.getRow();
     int col = currentPosition.getColumn();
 
     //lower right diagonal
     for (int j = col + 1, i = row + 1; j < 8 && i < 8; j++, i++) {
-        downRight.push_back(Coordinate(i, j));
+        lowerRight.push_back(Coordinate(i, j));
     }
 
     //lower left diagonal
     for (int j = col - 1, i = row + 1; j > -1 && i < 8; j--, i++) {
-        downLeft.push_back(Coordinate(i, j));
+        lowerLeft.push_back(Coordinate(i, j));
     }
 
     //upper left diagonal
     for (int j = col - 1, i = row - 1; j > -1 && i > -1; j--, i--) {
-        upLeft.push_back(Coordinate(i, j));
+        upperLeft.push_back(Coordinate(i, j));
     }
 
     //upper right diagonal
     for (int j = col + 1, i = row - 1; j < 8 && i > -1; j++, i--) {
-        upRight.push_back(Coordinate(i, j));
+        upperRight.push_back(Coordinate(i, j));
     }
 
     std::vector< std::vector<Coordinate> > result;
 
-    if (upLeft.size() != 0)
-        result.push_back(upLeft);
-    if (upRight.size() != 0)
-        result.push_back(upRight);
-    if (downLeft.size() != 0)
-        result.push_back(downLeft);
-    if (downRight.size() != 0)
-        result.push_back(downRight);
+    if (upperLeft.size() != 0)
+        result.push_back(upperLeft);
+    if (upperRight.size() != 0)
+        result.push_back(upperRight);
+    if (lowerLeft.size() != 0)
+        result.push_back(lowerLeft);
+    if (lowerRight.size() != 0)
+        result.push_back(lowerRight);
 
     return result;
 }
